4c031121622ec22f130d8778f4f657c5af61fcce
root <root@localhost.localdomain>
2021-02-02 16:17:54 +0000

    update app

--- runanalysisapp.py
+++ runanalysisapp.py
@@ -15,8 +15,29 @@ files = os.listdir(cwd)  # Get all the files in that directory
 print("Files in %r: %s" % (cwd, files))
 
 
+file1 = open('fractalbreakAllBuy[DJI30].txt', 'r')
+Lines = file1.readlines()
+
+count = 0
+v = []
+count = []
+counter = 0
+
+# Strips the newline character
+for line in Lines:
+    vlaue = (line.strip())
+    vlaue = vlaue[20:len(vlaue)]
+
+    #print(vlaue)
+    v.append(float(vlaue.strip()))
+    counter = counter + 1
+    #print(counter)
+    count.append(counter)
+
+
+
 #---------------------------------------------
-file4 = open("[DJI30]16385buyFractal.txt", "r")
+file4 = open("OneHourFractalRelativeToBid[DJI30].txt", "r")
 Line4 = file4.readlines()
 
 count4 = 0
@@ -32,7 +53,8 @@ for line in Line4:
 
 #---------------------------------------------
 st.write("""
-# Simple Stock Price App
+# Quantitative Analysis of DJI
+# Price App
 Shown are the stock **closing price** and ***volume*** of DJI!
 """)
 
@@ -42,12 +64,19 @@ tickerSymbol = 'DJI'
 #get data on this ticker
 tickerData = yf.Ticker(tickerSymbol)
 #get the historical prices for this ticker
-tickerDf = tickerData.history(period='1d', start='2010-5-31', end='2021-5-31')
+tickerDf = tickerData.history(period='1d', start='2020-1-31', end='2021-5-31')
 # Open	High	Low	Close	Volume	Dividends	Stock Splits
 
+st.write("""
+## Live: Bid Relative To 1H Fractal movements as percentage
+""")
+st.write("""
+ Current Value: 
+""", round(v[-1]),"""%""")
+st.line_chart(v[-100:])
 
 st.write("""
-## Fractal buy 1 hour
+## Bullish Fractal Breakout range 1 Hour
 """)
 st.line_chart(v4)
 
@@ -61,3 +90,9 @@ st.write("""
 """)
 st.line_chart(tickerDf.Volume)
 
+#file updated
+#file updated
+#file updated
+#file updated
+#file updated
+#file updated
